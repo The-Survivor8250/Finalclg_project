5c3a74a14b8590db9b2ec4b5d8aa72ce5857a485
The-Survivor8250 <rahul.yctc@gmail.com>
2024-07-18 22:58:57 +0530

    updated

--- bookinstatus.php
+++ bookinstatus.php
@@ -53,7 +53,7 @@ body{
     height: 40px;
     background: #ff7200;
     border:none;
-    margin-top: ;
+    margin-top: 0px;
     font-size: 18px;
     border-radius: 10px;
     cursor: pointer;
@@ -72,8 +72,8 @@ body{
     cursor: pointer;
     color:#fff;
     transition: 0.4s ease;
-    margin-top: ;
-    margin-left: ;
+    margin-top:0px ;
+    margin-left: 0px;
 }
 .utton:hover{
     color: black;
